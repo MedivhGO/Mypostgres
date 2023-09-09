956d8073c7c26a45f7cecc5dbe1c1679c70e819f
MedivhGO <lijingqi93@gmail.com>
2023-09-09 13:59:47 +0800

    add myfilereader

---
+++ cmudb/extensions/db721_fdw/src/myfilereader.h
@@ -0,0 +1,144 @@
+#pragma once
+
+#include <fstream>
+
+namespace myutil {
+
+class FileReader {
+ public:
+  FileReader();
+
+  ~FileReader();
+
+  auto Open(const std::string &file_path) -> bool;
+
+  auto HasOpen() -> bool;
+
+  auto HasEnd() -> bool;
+
+  auto Seek(std::streamoff offset, std::ios_base::seekdir way) -> FileReader&;
+
+  auto GetLength() -> size_t;
+
+  auto SetLength() -> void;
+
+  auto Tell() -> size_t;
+
+  auto Close() -> void;
+
+  auto Read4Bytes(char* dst) -> void {
+    fin_.read(dst, 4);
+  }
+
+  auto Read4BytesBE(char* dst) -> void {
+    this->fin_.read(dst + 3, 1);
+    this->fin_.read(dst + 2, 1);
+    this->fin_.read(dst + 1, 1);
+    this->fin_.read(dst, 1);
+  }
+
+  auto Read1Bytes(char* dst) -> void {
+    this->fin_.read(dst, 1);
+  }
+
+  auto ReadUInt8() -> uint8_t {
+    uint8_t data;
+    Read1Bytes(reinterpret_cast<char*>(&data));
+    return data;
+  }
+
+  auto ReadUInt32() -> uint32_t {
+    uint32_t data;
+    Read4Bytes(reinterpret_cast<char*>(&data));
+    return data;
+  }
+
+  auto ReadUInt32BE() -> uint32_t {
+    uint32_t data;
+    Read4BytesBE(reinterpret_cast<char*>(&data));
+    return data;
+  }
+
+  auto ReadUInt8Array(size_t count) {
+    uint8_t* data = new uint8_t[count];
+    for (size_t x = 0; x < count; x++) {
+      data[x] = ReadUInt8();
+    }
+    return data;
+  }
+
+  auto ReadAsciiString(size_t len) -> std::string {
+    char* buffer = (char*)ReadUInt8Array(len);
+    std::string data = std::string(buffer, len);
+    delete[] buffer;
+    return data;
+  }
+
+ private:
+  std::string file_path_;
+  std::ifstream fin_;
+  bool has_open_;
+  bool has_end_;
+  size_t length_;
+};
+
+FileReader::FileReader() {
+  file_path_ = "";
+  has_open_ = false;
+  has_end_ = false;
+}
+
+void FileReader::Close() {
+  if (has_open_) {
+    fin_.close();
+  }
+}
+
+FileReader::~FileReader() {
+  if (has_open_) {
+    fin_.close();
+  }
+}
+
+auto FileReader::Open(const std::string &directory) -> bool {
+  if (fin_.is_open()) {
+    fin_.close();
+  }
+
+  file_path_ = directory;
+  fin_.open(directory, std::ifstream::in | std::ifstream::binary);
+  
+  if (fin_.fail()) {
+    throw std::runtime_error("File does not exist");
+  }
+
+  this->SetLength();
+  this->Seek(0, std::ios_base::beg);
+
+  has_open_ = fin_.is_open();
+  return has_open_;
+}
+
+auto FileReader::Seek(std::streamoff offset, std::ios_base::seekdir way) -> FileReader& {
+    fin_.seekg(offset, way);
+    return *this;
+}
+
+auto FileReader::SetLength() -> void {
+  this->Seek(0, std::ios_base::end);
+  this->length_ = this->Tell();
+}
+
+auto FileReader::GetLength() -> size_t {
+  return length_;
+}
+
+auto FileReader::Tell() -> size_t {
+  return (size_t)this->fin_.tellg();
+}
+
+auto FileReader::HasOpen() -> bool { return has_open_; }
+
+auto FileReader::HasEnd() -> bool { return has_end_; }
+
+} // end mytuil namespace
--- cmudb/extensions/db721_fdw/src/myjson.h
+++ cmudb/extensions/db721_fdw/src/myjson.h
@@ -10,37 +10,145 @@
 #include <charconv>
 #include <memory>
 
-namespace jsontuil {
+namespace myutil {
 
 struct JSONObject;
 
-using JSONDict = std::unordered_map<std::string, std::unique_ptr<JSONObject>>;
+using JSONDict = std::unordered_map<std::string, std::shared_ptr<JSONObject>>;
 using JSONList = std::vector<JSONObject>;
 
 struct JSONObject {
     std::variant
-    < std::nullptr_t  // null
-    , bool            // true
-    , int             // 42
-    , double          // 3.14
-    , std::string     // "hello"
-    , JSONList        // [42, "hello"]
-    , JSONDict        // {"hello": 985, "world": 211}
+    < std::nullptr_t
+    , int
+    , float
+    , std::string
+    , JSONDict
     > inner;
+
+    template <class T>
+    bool is() const {
+        return std::holds_alternative<T>(inner);
+    }
+
+    template <class T>
+    T const &get() const {
+        return std::get<T>(inner);
+    }
+
+    template <class T>
+    T &get() {
+        return std::get<T>(inner);
+    }
 };
 
-JSONObject parse(std::string_view json) {
-    if (json.empty()) {
-        return JSONObject{std::nullptr_t{}};
+char unescaped_char(char c) {
+    switch (c) {
+    case 'n': return '\n';
+    case 'r': return '\r';
+    case '0': return '\0';
+    case 't': return '\t';
+    case 'v': return '\v';
+    case 'f': return '\f';
+    case 'b': return '\b';
+    case 'a': return '\a';
+    default: return c;
     }
-    if ('0' <= json[0] && json[0] <= '9') {
-        int val = json[0];
-        return JSONObject{int{val - '0'}};
-    }
-    return JSONObject{std::nullptr_t{}};
 }
 
+template <class T>
+std::optional<T> try_parse_num(std::string_view str) {
+    T value;
+    auto res = std::from_chars(str.data(), str.data() + str.size(), value);
+    if (res.ec == std::errc() && res.ptr == str.data() + str.size()) {
+        return value;
+    }
+    return std::nullopt;
+}
 
+std::pair<JSONObject, size_t> parse(std::string_view json) {
+    if (json.empty()) {
+        return {JSONObject{std::nullptr_t{}}, 0};
+    }
+
+    if (('0' <= json[0] && json[0] <= '9') || json[0] == '+' || json[0] == '-') { // maybe num
+        std::regex num_re{"[+-]?[0-9]+(\\.[0-9]*)?([eE][+-]?[0-9]+)?"};
+        std::cmatch match;
+        if (std::regex_search(json.data(), json.data() + json.size(), match, num_re)) {
+            std::string str = match.str();
+            if (auto num = try_parse_num<int>(str); num.has_value()) { // C++ 17 grammar
+                return {JSONObject{num.value()}, str.size()};
+            }
+            // if (auto num = try_parse_num<double>(str); (bool)num)
+            // if (auto num = try_parse_num<double>(str); num)
+            if (auto num = try_parse_num<float>(str)) {
+                return {JSONObject{*num}, str.size()};
+            }
+        }
+    } else if (size_t off = json.find_first_not_of(" \n\r\t\v\f\0"); off != 0 && off != json.npos) {
+        auto [obj, eaten] = parse(json.substr(off));
+        return {std::move(obj), eaten + off};
+    } else if ('"' == json[0]) {
+        std::string str;
+        enum {
+            Raw,
+            Escaped,
+        } phase = Raw;
+        size_t i;
+        for (i = 1; i < json.size(); ++i) {
+            char ch = json[i];
+            if (phase == Raw) {
+                if (ch == '\\') {
+                    phase = Escaped;
+                } else if (ch == '"') {
+                    i += 1;
+                    break;
+                } else {
+                    str += ch;
+                }
+            } else if (phase == Escaped) {
+                str += unescaped_char(ch);
+                phase = Raw;
+            } 
+        }
+        return {JSONObject{std::move(str)}, i};
+    }  else if  (json[0] == '{') {
+        std::unordered_map<std::string, std::shared_ptr<JSONObject>> res;
+        size_t i;
+        for (i = 1; i < json.size();) {
+            if (json[i] == '}') {
+                i += 1;
+                break;
+            }
+            auto [keyobj, keyeaten] = parse(json.substr(i));
+            if (keyeaten == 0) {
+                i = 0;
+                break;
+            }
+            i += keyeaten;
+            if (!std::holds_alternative<std::string>(keyobj.inner)) {
+                i = 0;
+                break;
+            }
+            if (json[i] == ':') {
+                i += 1;
+            }
+            std::string key = std::move(std::get<std::string>(keyobj.inner));
+            auto [valobj, valeaten] = parse(json.substr(i));
+            if (valeaten == 0) {
+                i = 0;
+                break;
+            }
+            i += valeaten;
+            res.try_emplace(std::move(key), std::make_shared<JSONObject>(valobj));
+            if (json[i] == ',') {
+                i += 1;
+            }
+        }
+        return {JSONObject{std::move(res)}, i};
+    }
+    return {JSONObject{std::nullptr_t{}}, 0};
+}
 
 } // end jsonutil namespace
 
